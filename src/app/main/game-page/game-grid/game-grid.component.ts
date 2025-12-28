49c0295bacbcba55b793f364e32312d59dcce445
Vit Tiupa <vitaliy.t@zazmic.com>
2025-12-28 09:57:28 +0100

    Added greed component

---
+++ src/app/main/game-page/game-grid/game-grid.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'fil-game-grid',
+  imports: [],
+  templateUrl: './game-grid.component.html',
+  styleUrl: './game-grid.component.scss'
+})
+export class GameGridComponent {
+
+}
