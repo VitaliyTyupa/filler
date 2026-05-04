0b6ed58fd6b63f81a30f7548df1a7c64b2c21a29
Vit Tiupa <vitaliy.t@zazmic.com>
2026-05-04 21:40:19 +0200

    added Auth

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,13 +5,14 @@ import { WaitingPageComponent } from './waiting-page/waiting-page.component';
 import { FinalPageComponent } from './final-page/final-page.component';
 import { LoginPageComponent } from './auth/login-page/login-page.component';
 import { RegisterPageComponent } from './auth/register-page/register-page.component';
+import { anonymousOnlyGuard, authRequiredGuard } from './auth/auth.guards';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/start', pathMatch: 'full' },
-  { path: 'login', component: LoginPageComponent },
-  { path: 'register', component: RegisterPageComponent },
-  { path: 'start', component: StartPageComponent },
-  { path: 'game', component: GamePageComponent },
-  { path: 'waiting', component: WaitingPageComponent },
-  { path: 'final', component: FinalPageComponent }
+  { path: 'login', component: LoginPageComponent, canActivate: [anonymousOnlyGuard] },
+  { path: 'register', component: RegisterPageComponent, canActivate: [anonymousOnlyGuard] },
+  { path: 'start', component: StartPageComponent, canActivate: [authRequiredGuard] },
+  { path: 'game', component: GamePageComponent, canActivate: [authRequiredGuard] },
+  { path: 'waiting', component: WaitingPageComponent, canActivate: [authRequiredGuard] },
+  { path: 'final', component: FinalPageComponent, canActivate: [authRequiredGuard] }
 ];
